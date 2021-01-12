c01dd8dc8f662ad2b6c3634fa386780de205c26d
jackbowen <jackbowen92@gmail.com>
2021-01-12 02:03:32 -0800

    Update baseFlock.js

--- js/baseFlock.js
+++ js/baseFlock.js
@@ -4,7 +4,8 @@ var flockSketch = function ( f ) {
   var scaledRodDiameter;
   var rods = [];
 
-  //var numRods = 12;
+  var maxSpeed = .5;
+  var maxForce = 0.03;
 
   f.preload = () => {
   }
@@ -28,7 +29,7 @@ var flockSketch = function ( f ) {
     }
     else if (!loadedBaseFlag) {
       loadBase();
-      for (var i = 0; i < 2; i++) {
+      for (var i = 0; i < 10; i++) {
         rods[i] = createRod();
       }
     }
@@ -47,42 +48,132 @@ var flockSketch = function ( f ) {
   function updateRods() {
     for (var i = 0; i < rods.length; i++) {
       var tempRod = rods[i];
-      // Add random wander
+      //wander(tempRod);
+      var edge = avoidEdge(tempRod);
+      tempRod.acc.add(edge);
+      
+      var sep = separate(tempRod);
+      tempRod.acc.add(sep);
+
+      // Add vel
+      tempRod.vel.add(tempRod.acc);
+      tempRod.vel.limit(maxSpeed);
+      tempRod.loc.add(tempRod.vel);
+      //tempRod.loc.add(tempRod.vel);
+    }
+  }
+
+  function wander(rod) {
+          // Add random wander
       //rod.x += rodSpeed * (f.noise(rodNoise)-.5);
       //rod.y += rodSpeed * (f.noise(rodNoise+20)-.5);
       //var wander = f.createVector()
       //rodNoise += rodNoiseInc;
+  }
 
-      // Avoid shape edge 
-      // Determine closest edge
-      var closestEdge = [0,1];
-      var closestDist = 10000000;
-      for (var j = 0; j < basePoints.length - 1; j++) {
-        //console.log("gh");
-        var p1Dist = f.dist(tempRod.loc.x, tempRod.loc.y, basePoints[j].x, basePoints[j].y);
-        var p2Dist = f.dist(tempRod.loc.x, tempRod.loc.y, basePoints[j+1].x, basePoints[j+1].y);
-        var totalDist = p1Dist + p2Dist;
-        //console.log(totalDist);
-        if (totalDist < closestDist) {
-          closestEdge = [j, j+1];
-          closestDist = totalDist;
-        }
+  // Avoid shape edge 
+  function avoidEdge(rod) {
+    // Determine closest edge
+    var closestEdge = [0,1];
+    var closestDist = 10000000;
+      
+    for (var j = 0; j < basePoints.length - 1; j++) {
+      //console.log("gh");
+      var p1Dist = f.dist(rod.loc.x, rod.loc.y, basePoints[j].x, basePoints[j].y);
+      var p2Dist = f.dist(rod.loc.x, rod.loc.y, basePoints[j+1].x, basePoints[j+1].y);
+      var totalDist = p1Dist + p2Dist;
+      //console.log(totalDist);
+      if (totalDist < closestDist) {
+        closestEdge = [j, j+1];
+        closestDist = totalDist;
       }
-
-      // DEBUGGING
-      f.push();
-      f.translate(f.width/2, f.height/2);
-      console.log(closestEdge);
-      f.strokeWeight(3);
-      f.stroke(255, 0, 0);
-      f.line(basePoints[closestEdge[0]].x, basePoints[closestEdge[0]].y, basePoints[closestEdge[1]].x, basePoints[closestEdge[1]].y);
-      f.strokeWeight(1);
-      f.stroke(0);
-      f.pop();
-
-      // Add vel
-      tempRod.loc.add(tempRod.vel);
     }
+    var p1 = {x: basePoints[closestEdge[0]].x, y: basePoints[closestEdge[0]].y};
+    var p2 = {x: basePoints[closestEdge[1]].x, y: basePoints[closestEdge[1]].y}
+    var distToLine = distanceToLine(rod.loc.x, rod.loc.y, p1.x, p1.y, p2.x, p2.y);
+
+
+    // DEBUGGING
+    f.push();
+    f.translate(f.width/2, f.height/2);
+    //console.log(closestEdge);
+    f.strokeWeight(3);
+    f.stroke(255, 0, 0);
+    if (distToLine < 50) {
+      f.stroke(0, 255, 0);
+      var normalVector = f.createVector(p1.y - p2.y, -(p1.x - p2.x));
+      f.line(rod.loc.x, rod.loc.y, rod.loc.x + normalVector.x, rod.loc.y + normalVector.y);
+    }
+    f.line(p1.x, p1.y, p2.x, p2.y);
+    f.strokeWeight(1);
+    f.stroke(0);
+    f.pop();
+
+    var steer = f.createVector(0, 0);
+    var desiredMargin = scaleFactor * 4;
+    if (distToLine < desiredMargin) {
+      var diff = f.createVector(p1.y - p2.y, -(p1.x - p2.x));
+      diff.normalize();
+      diff.div(distToLine);
+      steer.add(diff);
+    }
+
+     if (steer.mag() > 0) {
+      // First two lines of code below could be condensed with new PVector setMag() method
+      // Not using this method until Processing.js catches up
+      // steer.setMag(maxspeed);
+
+      // Implement Reynolds: Steering = Desired - Velocity
+      steer.normalize();
+      steer.mult(maxSpeed);
+      steer.sub(rod.vel);
+      steer.limit(maxForce);
+    }
+    return steer;
+  }
+
+  function separate(rod) {
+    var desiredSeparation = scaleFactor * 3;
+    var steer = f.createVector(0, 0);
+    var count = 0;
+
+    // For every rod in the system, check if it's too close
+    for (var i = 0; i < rods.length; i++) {
+      var other = rods[i]
+      var d = f.dist(rod.loc.x, rod.loc.y, other.loc.x, other.loc.y);
+      // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+      if ((d > 0) && (d < desiredSeparation)) {
+        // Calculate vector pointing away from neighbor
+        var diff = p5.Vector.sub(rod.loc, other.loc);
+        diff.normalize();
+        diff.div(d);        // Weight by distance
+        steer.add(diff);
+        count++;            // Keep track of how many
+      }
+    }
+    // Average -- divide by how many
+    if (count > 0) {
+      steer.div(count);
+    }
+
+    // As long as the vector is greater than 0
+    if (steer.mag() > 0) {
+      // First two lines of code below could be condensed with new PVector setMag() method
+      // Not using this method until Processing.js catches up
+      // steer.setMag(maxspeed);
+
+      // Implement Reynolds: Steering = Desired - Velocity
+      steer.normalize();
+      steer.mult(maxSpeed);
+      steer.sub(rod.vel);
+      steer.limit(maxForce);
+    }
+    return steer;
+  }
+
+  function distanceToLine(x0, y0, x1, y1, x2, y2) {
+    //console.log(x0 + ', ' + y0 + ', ' + x1 + ', ' + y1 + ', ' + x2 + ', ' + y2);
+    return (f.abs((x2-x1)*(y1-y0) - (x1-x0)*(y2-y1))) / (f.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
   }
 
   function drawRods() {
@@ -97,7 +188,8 @@ var flockSketch = function ( f ) {
 
   function createRod() {
     return {loc: f.createVector(f.random(-10, 10), f.random(-10, 10)), 
-            vel: f.createVector(f.random(-.2,.2), f.random(-.2,.2))};
+            vel: f.createVector(f.random(-.2,.2), f.random(-.2,.2)),
+            acc: f.createVector(0, 0)};
   }
 
   function loadBase() {
